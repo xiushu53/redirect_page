072136623f8a655698f1f19c6eb5ca0e8406950a
xiushu53 <xiushu@me.com>
2017-05-25 17:40:47 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,6 +4,9 @@ console.log(ua);
 if (ua.match(/iPhone|iPad/)) {
              window.location.href = "comgooglemaps://?daddr=35.726700,139.716695&directionsmode=transit";
              }
-             else {
-             window.location.href = "maps.google.com/maps?daddr=35.726700,139.716695&directionsmode=transit";
-             };
+else if (ua.match(/Android/)) {
+  window.location.href = "maps.google.com/maps?daddr=35.726700,139.716695&directionsmode=transit";
+             }
+else {
+  window.location.href = "https://maps.google.com/maps?daddr=35.726700,139.716695&directionsmode=transit"
+};
