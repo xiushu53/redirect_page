99015e0672f7b75b87530a2d5a83e7698d38a828
xiushu53 <xiushu@me.com>
2017-05-25 16:11:53 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,3 +1,9 @@
 var ua = navigator.userAgent;
 console.log(ua);
-//window.location.href = "comgooglemaps://?daddr=35.726700,139.716695&directionsmode=transit";
+
+if (ua.match(/iPhone|iPad)) {
+             window.location.href = "comgooglemaps://?daddr=35.726700,139.716695&directionsmode=transit";
+             }
+             else {
+             window.location.href = "maps.google.com/maps?daddr=35.726700,139.716695&directionsmode=transit";
+             };
