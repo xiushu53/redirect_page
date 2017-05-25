9e5e9f5a97a1fd6f275e0bb6dcc49b93eb9459c2
xiushu53 <xiushu@me.com>
2017-05-25 16:06:15 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,3 @@
-window.location.href = "comgooglemaps://?daddr=35.726700,139.716695&directionsmode=transit";
+var ua = navigator.userAgent;
+console.log(ua);
+//window.location.href = "comgooglemaps://?daddr=35.726700,139.716695&directionsmode=transit";
